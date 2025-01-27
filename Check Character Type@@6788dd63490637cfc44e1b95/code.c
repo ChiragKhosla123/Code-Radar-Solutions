daeca1b754cc888934baa78032c55e8380f69262
ChiragKhosla123 <chirag0220.becse24@chitkara.edu.in>
2025-01-27 22:04:25 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -8,7 +8,7 @@ int main() {
         printf("Vowel");
     }
     else if(isalpha(a)){
-        printf("Consonent");
+        printf("Consonant");
     }
     else if(isdigit(a)){
         printf("Digit");
