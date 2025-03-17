76e7d8b37612da91bb890524a000215da314acc2
ChiragKhosla123 <chirag0220.becse24@chitkara.edu.in>
2025-03-17 14:01:24 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,30 +1,20 @@
-#include<stdio.h>
-int main(){
-    int n,t,s,z;
-    scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-    for(int i=0;i<n;i++){
-        for(int i=0;i<n;i++){
-            s=0;
-            z=a[i];
-            while(0<z){
-                t=z%10;
-                z=z/10;
-                s=s*10+t;
+#include <stdio.h>
+#include <string.h>
 
-            }
-            }
-            if(s==a[i]){
-                printf("YES");
-                break;
-            }
-            else{
-                printf("NO");
-                break;
-            }
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+
+    // Read input numbers
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
-    return 0;
-}
+
+    char combined[1000] = ""; // To store concatenated number as string
+    char temp[20]; // Temporary storage for each number
+
+    // Concatenate numbers into a single string
+    for (int i = 0; i < n; i++) {
+        sprintf(temp, "%d", a[i]); // Convert number to string
+        strcat(combined, temp); // Append to the combined
