6554e2c5ecb7ffa07283064530f84a8d428e102d
ChiragKhosla123 <chirag0220.becse24@chitkara.edu.in>
2025-03-18 00:27:01 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,37 +1,30 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    
-    char combined[1000] = ""; // Buffer to store concatenated number as string
-    char temp[20]; // Temporary buffer for each number
-
-    // Read input numbers as strings and concatenate with spaces
-    for (int i = 0; i < n; i++) {
-        scanf("%s", temp); // Read input as string
-        if (i > 0) strcat(combined, " "); // Add space between numbers
-        strcat(combined, temp); // Append to combined string
+#include<stdio.h>
+int main(){
+    int n,t,s,z;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
     }
+    for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
+            s=0;
+            z=a[i];
+            while(0<z){
+                t=z%10;
+                z=z/10;
+                s=s*10+t;
 
-    // Check if the concatenated string is a palindrome (including spaces)
-    int len = strlen(combined);
-    int isPalindrome = 1; // Flag for palindrome check
-
-    for (int i = 0; i < len / 2; i++) {
-        if (combined[i] != combined[len - i - 1]) {
-            isPalindrome = 0; // Not a palindrome
-            break;
-        }
+            }
+            }
+            if(s==a[i]){
+                printf("YES");
+                break;
+            }
+            else{
+                printf("NO");
+                break;
+            }
     }
-
-    // Print the result
-    if (isPalindrome) {
-        printf("YES\n");
-    } else {
-        printf("NO\n");
-    }
-
     return 0;
 }
