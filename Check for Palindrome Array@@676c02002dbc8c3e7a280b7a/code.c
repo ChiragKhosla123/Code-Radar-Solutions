d24fa442a1d5ca9399e0fd7a2d948b89f9e77cdd
ChiragKhosla123 <chirag0220.becse24@chitkara.edu.in>
2025-03-22 01:08:53 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -12,7 +12,7 @@ int main(){
                 printf("Yes");
             }
             else{
-                printf("No")
+                printf("No");
             }
             }//102010
     }
