3d0ccc39248a489688922e02fa8d1ed052faa218
ChiragKhosla123 <chirag0220.becse24@chitkara.edu.in>
2025-03-27 12:17:17 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
+int factorialRange(int end);
 
-int factorialRange(int end){
+
     if(end==1||end==0 ){
         return 1;
     }
     else{
         return end*(factorialRange(end-1));
     }
-}
